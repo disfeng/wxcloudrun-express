5cdec4b609882cc01529a38014776b7a36bc9731
junyi.chen <disfeng1986@163.com>
2023-05-25 16:45:45 +0800

    update

--- index.js
+++ index.js
@@ -1,54 +1,34 @@
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-// const { init: initDB, Counter } = require("./db");
-
-const logger = morgan("tiny");
-
+const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
-app.use(logger);
+const PORT = process.env.PORT || 80
+const HOST = '0.0.0.0'
 
-//使用axios
+// App
+const app = express()
+
+app.use(bodyParser.raw())
+app.use(bodyParser.json({}))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const client = axios.default
 
-//微信小程序消息推送
-app.post("/api/wx_push", async (req, res) => {
-  const headers = req.headers
-    const weixinAPI = `http://api.weixin.qq.com/cgi-bin/message/custom/send`;
+app.all('/', async (req, res) => {
+    const headers = req.headers
+    const weixinAPI = `http://api.weixin.qq.com/cgi-bin/message/custom/send`
     const payload = {
-      touser: headers['x-wx-openid'],
-      msgtype: 'text',
-      text: {
-          content: `云托管接收消息推送成功，内容如下：\n${JSON.stringify(req.body, null, 2)}`
-      }
-  }
-  // dispatch to wx server update
-  const result = await client.post(weixinAPI, payload)
-  console.log('received request', req.body, result.data)
-  res.send('success')
+        touser: headers['x-wx-openid'],
+        msgtype: 'text',
+        text: {
+            content: `云托管接收消息推送成功，内容如下：\n${JSON.stringify(req.body, null, 2)}`
+        }
+    }
+    // dispatch to wx server
+    const result = await client.post(weixinAPI, payload)
+    console.log('received request', req.body, result.data)
+    res.send('success')
 });
 
-// 小程序调用，获取微信 Open ID
-app.get("/api/wx_openid", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
-  }
-});
-
-const port = process.env.PORT || 80;
-
-async function bootstrap() {
-  // await initDB();
-  app.listen(port, () => {
-    console.log("启动成功", port);
-  });
-}
-
-bootstrap();
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
