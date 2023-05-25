78cec7909b5950793c33020fea08edcf3ecf8163
junyi.chen <disfeng1986@163.com>
2023-05-25 15:42:48 +0800

    增加推送消息

--- index.js
+++ index.js
@@ -12,34 +12,21 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
-// 首页
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
+//微信小程序消息推送
+app.post("/api/wx_push", async (req, res) => {
+  const headers = req.headers
+    const weixinAPI = `http://api.weixin.qq.com/cgi-bin/message/custom/send`;
+    const payload = {
+      touser: headers['x-wx-openid'],
+      msgtype: 'text',
+      text: {
+          content: `云托管接收消息推送成功，内容如下：\n${JSON.stringify(req.body, null, 2)}`
+      }
   }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
-});
-
-// 获取计数
-app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
+  // dispatch to wx server
+  const result = await client.post(weixinAPI, payload)
+  console.log('received request', req.body, result.data)
+  res.send('success')
 });
 
 // 小程序调用，获取微信 Open ID
